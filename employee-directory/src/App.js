6f4917a0ac32e34569ecfc783d3722276581fa5d
James Calverley <james.calverley3@gmail.com>
2020-04-03 20:23:36 -0400

    revises component structure

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './css/App.css';
 import Header from './components/Header';
-import EmployeeDirectory from './components/EmployeeDirectory';
+import Search from './components/Search';
+
 
 
 function App() {
   return (
     <>
       <Header />
-      <EmployeeDirectory />
+      <Search />
+      
     </>
   );
 }
+
+//! nest table within form and then pass the searchValue into it
+// pass down event.target.value
 
 export default App;
--- employee-directory/src/components/Search.js
+++ employee-directory/src/components/Search.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
 import '../css/Component-style.css';
+import Table from './Table';
 
-function Form() {
+function Search() {
   const [ searchValue, setSearchValue ] = useState([]);
   
 
@@ -9,17 +10,20 @@ function Form() {
     const input = event.target.value;
     console.log('[search input]', input)
     setSearchValue( input );
+    displayTable();
   };
+
+  function displayTable(){
+    console.log(">>>filter by:", searchValue )
+  }
 
   function handleSearch(){
     console.log('[SEARCH VALUE]', searchValue)
   };
 
-  
-  
-  
   return (
-    
+    //! use <form onSubmit={}> </form> rather than button on click. Will now work with enter
+    <>
     <form>
       <div class="input-group">
           <input onChange={handleInputChange} value={searchValue} type="text" class="form-control form-control-lg" placeholder="Search for an employee"/>
@@ -28,8 +32,24 @@ function Form() {
           </div>
       </div>
     </form> 
-    
+     <div class="table-wrapper">
+     <table class="table">
+       <thead class="thead-dark">
+         <tr>
+           <th scope="col">id</th>
+           <th scope="col">Name</th>
+           <th scope="col">username</th>
+           <th scope="col">Email</th>
+           <th scope="col">Phone#</th>
+         </tr>
+       </thead>
+       <tbody>
+         <Table />
+       </tbody>
+     </table>
+   </div>
+   </>
   );
 };
 
-export default Form;
+export default Search;
